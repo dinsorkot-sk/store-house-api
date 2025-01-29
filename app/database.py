065e99a7cfa67e6ed72318db63282d15ce174701
dinsorkot-sk <sk.dinsorkot@gmail.com>
2025-01-29 14:35:58 +0700

    update database .py

--- app/database.py
+++ app/database.py
@@ -7,7 +7,7 @@ import os
 
 load_dotenv()
 
-URL = f"mysql://{os.getenv("DB_USERNAME")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_POST")}/{os.getenv("DB_NAME")}"
+URL = f"mysql://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_POST')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
